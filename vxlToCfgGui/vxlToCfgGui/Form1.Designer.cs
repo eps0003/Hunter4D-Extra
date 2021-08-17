9aae33005d7d17f44627f6a826bd71bab350492b
epsilon <eps0003@users.noreply.github.com>
2021-08-17 16:33:38 +1000

    Rewrote GoldenGuy's program

---
+++ vxlToCfgGui/vxlToCfgGui/Form1.Designer.cs
@@ -0,0 +1,101 @@
+﻿
+namespace vxlToCfgGui
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.folderTree = new System.Windows.Forms.TreeView();
+            this.fileList = new System.Windows.Forms.ListView();
+            this.preview = new System.Windows.Forms.PictureBox();
+            this.exportButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.preview)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // folderTree
+            // 
+            this.folderTree.Location = new System.Drawing.Point(13, 13);
+            this.folderTree.Name = "folderTree";
+            this.folderTree.Size = new System.Drawing.Size(200, 230);
+            this.folderTree.TabIndex = 0;
+            this.folderTree.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.folderTree_AfterSelect);
+            // 
+            // fileList
+            // 
+            this.fileList.HideSelection = false;
+            this.fileList.Location = new System.Drawing.Point(12, 249);
+            this.fileList.Name = "fileList";
+            this.fileList.Size = new System.Drawing.Size(200, 230);
+            this.fileList.TabIndex = 1;
+            this.fileList.UseCompatibleStateImageBehavior = false;
+            this.fileList.View = System.Windows.Forms.View.List;
+            this.fileList.SelectedIndexChanged += new System.EventHandler(this.fileList_SelectedIndexChanged);
+            // 
+            // preview
+            // 
+            this.preview.BackColor = System.Drawing.SystemColors.Window;
+            this.preview.Location = new System.Drawing.Point(219, 12);
+            this.preview.Name = "preview";
+            this.preview.Size = new System.Drawing.Size(512, 512);
+            this.preview.TabIndex = 2;
+            this.preview.TabStop = false;
+            // 
+            // exportButton
+            // 
+            this.exportButton.Location = new System.Drawing.Point(13, 485);
+            this.exportButton.Name = "exportButton";
+            this.exportButton.Size = new System.Drawing.Size(199, 39);
+            this.exportButton.TabIndex = 3;
+            this.exportButton.Text = "Export";
+            this.exportButton.UseVisualStyleBackColor = true;
+            this.exportButton.Click += new System.EventHandler(this.exportButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(743, 536);
+            this.Controls.Add(this.exportButton);
+            this.Controls.Add(this.preview);
+            this.Controls.Add(this.fileList);
+            this.Controls.Add(this.folderTree);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.preview)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TreeView folderTree;
+        private System.Windows.Forms.ListView fileList;
+        private System.Windows.Forms.PictureBox preview;
+        private System.Windows.Forms.Button exportButton;
+    }
+}
+
---
+++ vxlToCfgGui/vxlToCfgGui/Form1.cs
@@ -0,0 +1,297 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+using static System.Windows.Forms.ListViewItem;
+
+namespace vxlToCfgGui
+{
+    public partial class Form1 : Form
+    {
+        private string filePath = "";
+        private string fileName = "";
+        private byte[] fileBytes;
+        private int[,,] map = new int[512, 64, 512];
+        private Color[,,] color = new Color[512, 64, 512];
+        private Color defaultBlockColor = Color.FromArgb(103, 64, 30);
+        private Bitmap img;
+
+        public Form1()
+        {
+            InitializeComponent();
+            PopulateTreeView();
+        }
+
+        private void PopulateTreeView()
+        {
+            DirectoryInfo info = new DirectoryInfo(@"../../..");
+            if (info.Exists)
+            {
+                TreeNode rootNode = new TreeNode(info.Name);
+                rootNode.Tag = info;
+                GetDirectories(info.GetDirectories(), rootNode);
+                folderTree.Nodes.Add(rootNode);
+            }
+        }
+
+        private void GetDirectories(DirectoryInfo[] subDirs, TreeNode parentNode)
+        {
+            DirectoryInfo[] subSubDirs;
+            foreach (DirectoryInfo subDir in subDirs)
+            {
+                TreeNode node = new TreeNode(subDir.Name, 0, 0);
+                node.Tag = subDir;
+                //node.ImageKey = "folder";
+                subSubDirs = subDir.GetDirectories();
+
+                parentNode.Nodes.Add(node);
+
+                if (subSubDirs.Length > 0)
+                {
+                    GetDirectories(subSubDirs, node);
+                }
+            }
+        }
+
+        private void folderTree_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+            fileList.Items.Clear();
+
+            DirectoryInfo nodeDirInfo = (DirectoryInfo)e.Node.Tag;
+
+            foreach (FileInfo file in nodeDirInfo.GetFiles())
+            {
+                if (file.Extension != ".vxl") continue;
+
+                ListViewItem item = new ListViewItem(file.Name, 1);
+                ListViewSubItem[] subItems = new ListViewSubItem[]
+                {
+                    new ListViewSubItem(item, file.DirectoryName)
+                };
+
+                item.SubItems.AddRange(subItems);
+                fileList.Items.Add(item);
+            }
+
+            fileList.AutoResizeColumns(ColumnHeaderAutoResizeStyle.HeaderSize);
+        }
+
+        private void fileList_SelectedIndexChanged(object sender, System.EventArgs e)
+        {
+            preview.Image = null;
+
+            if (fileList.SelectedItems.Count > 0)
+            {
+                filePath = fileList.SelectedItems[0].SubItems[1].Text;
+                fileName = fileList.SelectedItems[0].Text;
+
+                fileBytes = File.ReadAllBytes(filePath + "/" + fileName);
+
+                new Thread(new ThreadStart(LoadFile)).Start();
+            }
+        }
+
+        private void LoadFile()
+        {
+            void SetGeom(int x, int y, int z, int t)
+            {
+                if (y < 0 || y >= 64)
+                {
+                    MessageBox.Show("setgeom z is oob: " + y);
+                    Thread.CurrentThread.Abort();
+                    return;
+                }
+
+                map[x, 63 - y, z] = t;
+            }
+
+            void SetColor(int x, int y, int z, Color c)
+            {
+                if (y < 0 || y >= 64)
+                {
+                    MessageBox.Show("setcolor z is oob: " + y);
+                    Thread.CurrentThread.Abort();
+                    return;
+                }
+
+                color[x, 63 - y, z] = c;
+            }
+
+            {
+                int v = 0;
+                int y;
+                for (int z = 0; z < 512; ++z)
+                {
+                    for (int x = 0; x < 512; ++x)
+                    {
+                        for (y = 0; y < 64; ++y)
+                        {
+                            SetGeom(x, y, z, 1);
+                            SetColor(x, y, z, defaultBlockColor);
+                        }
+                        y = 0;
+                        while (true)
+                        {
+                            int number_4byte_chunks = fileBytes[v + 0];
+                            int top_color_start = fileBytes[v + 1];
+                            int top_color_end = fileBytes[v + 2];
+
+                            for (int i = y; i < top_color_start; i++)
+                                SetGeom(x, i, z, 0);
+
+                            int color_index = v + 4;
+                            for (y = top_color_start; y <= top_color_end; y++)
+                            {
+                                SetColor(x, y, z, Color.FromArgb(fileBytes[color_index + 2], fileBytes[color_index + 1], fileBytes[color_index]));
+                                color_index += 4;
+                            }
+
+                            int len_bottom = top_color_end - top_color_start + 1;
+
+                            // check for end of data marker
+                            if (number_4byte_chunks == 0)
+                            {
+                                // infer ACTUAL number of 4-byte chunks from the length of the color data
+                                v += 4 * (len_bottom + 1);
+                                break;
+                            }
+
+                            // infer the number of bottom colors in next span from chunk length
+                            int len_top = (number_4byte_chunks - 1) - len_bottom;
+
+                            // now skip the v pointer past the data to the beginning of the next span
+                            v += number_4byte_chunks * 4;
+
+                            int bottom_color_end = fileBytes[v + 3]; // aka air start
+                            int bottom_color_start = bottom_color_end - len_top;
+
+                            for (y = bottom_color_start; y < bottom_color_end; ++y)
+                            {
+                                SetColor(x, y, z, Color.FromArgb(fileBytes[color_index + 2], fileBytes[color_index + 1], fileBytes[color_index]));
+                                color_index += 4;
+                            }
+                        }
+                    }
+                }
+            }
+
+            UpdatePreview();
+        }
+
+        private void UpdatePreview()
+        {
+            img = new Bitmap(512, 512);
+
+            for (int x = 0; x < 512; x++)
+            {
+                for (int z = 0; z < 512; z++)
+                {
+                    for (int y = 63; y >= 0; y--)
+                    {
+                        if (map[x, y, z] == 0) continue;
+
+                        Color col = color[x, y, z];
+                        img.SetPixel(x, z, col);
+                        break;
+                    }
+                }
+            }
+
+            preview.Image = img;
+        }
+
+        private void exportButton_Click(object sender, EventArgs e)
+        {
+            if (fileName != "")
+            {
+                new Thread(new ThreadStart(ExportFile)).Start();
+            }
+        }
+
+        private string ToBase(int value, char[] baseChars)
+        {
+            // 32 is the worst cast buffer size for base 2 and int.MaxValue
+            int i = 32;
+            char[] buffer = new char[i];
+            int targetBase = baseChars.Length;
+
+            do
+            {
+                buffer[--i] = baseChars[value % targetBase];
+                value = value / targetBase;
+            }
+            while (value > 0);
+
+            char[] result = new char[32 - i];
+            Array.Copy(buffer, i, result, 0, 32 - i);
+
+            return new string(result);
+        }
+
+        private char[] baseChars = {
+            '0','1','2','3','4','5','6','7','8','9',
+            'a','b','c','d','e','f'/*,'g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z',
+            'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'*/
+        };
+
+        public void ExportFile()
+        {
+            uint EncodeColor(Color color)
+            {
+                return (uint)((color.R << 17) | (color.G << 9) | (color.B << 1) | 1);
+            }
+
+            //string EncodeColor(Color color)
+            //{
+            //    return ToBase((color.R << 17) | (color.G << 9) | (color.B << 1) | 1, baseChars);
+            //}
+
+            MessageBox.Show("Exporting.");
+
+            StringBuilder data = new StringBuilder("blocks = 0;");
+
+            bool prevVisible = true;
+            int lastVisibleIndex = 0;
+            int i = 0;
+
+            for (int y = 0; y < 64; y++)
+            {
+                for (int z = 0; z < 512; z++)
+                {
+                    for (int x = 0; x < 512; x++)
+                    {
+                        bool visible = map[x, y, z] == 1;
+
+                        if (visible)
+                        {
+                            if (!prevVisible)
+                            {
+                                data.Append("0;");
+                                data.Append(i - lastVisibleIndex - 2);
+                                data.Append(";");
+                            }
+
+                            Color block = color[x, y, z];
+                            data.Append(EncodeColor(block));
+                            data.Append(";");
+
+                            lastVisibleIndex = i;
+                        }
+
+                        prevVisible = visible;
+                        i++;
+                    }
+                }
+            }
+
+            data.Append("\nsize = 512;64;512;\n");
+
+            string name = fileName.Remove(fileName.Length - 4);
+            File.WriteAllText(filePath + "/" + name + ".cfg", data.ToString());
+
+            MessageBox.Show("Done.");
+        }
+    }
+}
